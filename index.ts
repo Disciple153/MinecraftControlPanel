5297eb5f173dc5589c557318383e24b39f77850b
Canaan Epperson <cepperson16@georgefox.edu>
2020-04-29 03:44:59 -0700

    barebones

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 
-function minecraftServer(action) {
+function minecraftServer(action, element) {
     $.ajax({
         url: "https://q6h7w0vm2b.execute-api.us-west-2.amazonaws.com/minecraftServer",
         crossDomain: true,
         data: `action=${action}`,
         success: function (data) {
             console.log("Success: " + JSON.stringify(data));
-            $('#output').html(data['response']);
+            $('#' + element).html(data['response']);
         },
         error: function (x, y, z) {
             console.log("ERROR\n" + JSON.stringify(x) + "\n" + y + "\n" + z);
