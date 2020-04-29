27d596abf41fe5c7d8063fd3527580616e06cb55
Canaan Epperson <cepperson16@georgefox.edu>
2020-04-29 11:19:29 -0700

    adding dynamic power status updating

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
+var GET_POWER_STATE = 'getPowerState';
+var TURN_ON = 'turnOn';
+var TURN_OFF = 'turnOff';
+var GET_IP = 'getIP';
+var PowerState;
+(function (PowerState) {
+    PowerState["running"] = "running";
+    PowerState["pending"] = "pending";
+    PowerState["stopping"] = "stopping";
+    PowerState["stopped"] = "stopped";
+})(PowerState || (PowerState = {}));
+function main() {
+    minecraftServer(GET_POWER_STATE, 'powerState');
+}
+function turnOn(element) {
+    minecraftServer(TURN_ON).done(function (data) {
+        $('#' + element).html(data.response);
+        checkUntil(PowerState.running, element);
+    });
+}
+function turnOff(element) {
+    minecraftServer(TURN_OFF).done(function (data) {
+        $('#' + element).html(data.response);
+        checkUntil(PowerState.stopped, element);
+    });
+}
+function checkUntil(powerState, element) {
+    minecraftServer(GET_POWER_STATE).done(function (data) {
+        // Update power state        $('#' + element).html(data.response);
+        $('#' + element).html(data.response);
+        // If the target has been reached, update the IP address
+        if (data.response == powerState) {
+            minecraftServer(GET_IP, 'ipAddress');
+        }
+        // If powerState is not the target, try again
+        else {
+            checkUntil(powerState, element);
+        }
+    });
+}
 function minecraftServer(action, element) {
-    $.ajax({
+    if (element === void 0) { element = ''; }
+    return $.ajax({
         url: "https://q6h7w0vm2b.execute-api.us-west-2.amazonaws.com/minecraftServer",
         crossDomain: true,
         data: "action=" + action,
         success: function (data) {
             console.log("Success: " + JSON.stringify(data));
-            $('#' + element).html(data['response']);
+            if (element != '') {
+                $('#' + element).html(data['response']);
+            }
         },
         error: function (x, y, z) {
             console.log("ERROR\n" + JSON.stringify(x) + "\n" + y + "\n" + z);
--- index.ts
+++ index.ts
@@ -1,12 +1,64 @@
 
-function minecraftServer(action, element) {
-    $.ajax({
+const GET_POWER_STATE : string = 'getPowerState';
+const TURN_ON : string = 'turnOn';
+const TURN_OFF : string = 'turnOff';
+const GET_IP : string = 'getIP';
+
+enum PowerState {
+    running = 'running',
+    pending = 'pending',
+    stopping = 'stopping',
+    stopped = 'stopped'
+}
+
+function main(){
+    minecraftServer(GET_POWER_STATE, 'powerState');
+}
+
+function turnOn(element: string) {
+    minecraftServer(TURN_ON).done( function (data) {
+        $('#' + element).html(data.response);
+
+        checkUntil(PowerState.running, element);
+    });
+}
+
+function turnOff(element: string) {
+    minecraftServer(TURN_OFF).done( function (data) {
+        $('#' + element).html(data.response);
+
+        checkUntil(PowerState.stopped, element);
+    });
+}
+
+
+function checkUntil(powerState: PowerState, element: string) {
+    minecraftServer(GET_POWER_STATE).done(function (data) {
+        // Update power state        $('#' + element).html(data.response);
+        $('#' + element).html(data.response);
+
+        // If the target has been reached, update the IP address
+        if (data.response == powerState) {
+            minecraftServer(GET_IP, 'ipAddress');
+        }
+        // If powerState is not the target, try again
+        else {
+            checkUntil(powerState, element);
+        }
+    });
+}
+
+function minecraftServer(action: string, element: string = '') {
+    return $.ajax({
         url: "https://q6h7w0vm2b.execute-api.us-west-2.amazonaws.com/minecraftServer",
         crossDomain: true,
         data: `action=${action}`,
         success: function (data) {
             console.log("Success: " + JSON.stringify(data));
-            $('#' + element).html(data['response']);
+
+            if (element != '') {
+                $('#' + element).html(data['response']);
+            }
         },
         error: function (x, y, z) {
             console.log("ERROR\n" + JSON.stringify(x) + "\n" + y + "\n" + z);
